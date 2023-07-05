70cdaf476d6675a325ec964ff057210b8ce1314c
Roy <pipiroy03@gmail.com>
2023-07-05 02:00:44 -0700

    first commit

---
+++ Backend/Backend/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Models;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? IdTerm { get; set; }
+
+    public virtual Term? IdTermNavigation { get; set; }
+}
---
+++ Backend/Backend/Models/CoursesDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models;
+
+public partial class CoursesDbContext : DbContext
+{
+    public CoursesDbContext()
+    {
+    }
+
+    public CoursesDbContext(DbContextOptions<CoursesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Term> Terms { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3213E83FBD710038");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.IdTerm).HasColumnName("id_term");
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.IdTermNavigation).WithMany(p => p.Courses)
+                .HasForeignKey(d => d.IdTerm)
+                .HasConstraintName("FK__Courses__id_term__5EBF139D");
+        });
+
+        modelBuilder.Entity<Term>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Terms__3213E83FE2905438");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
